b61b41982bc1be0ba9b4afb3f8de62179fd5c0a3
zhouyijun <zhou495248579@hotmail.com>
2021-08-27 17:30:21 +0800

    hook test

--- test/hook.test.ts
+++ test/hook.test.ts
@@ -1,13 +1,35 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { act, renderHook } from "@testing-library/react-hooks";
 import { useCallback, useState } from "react";
-export default function useCounter() {
-  const [count, setCount] = useState(0);
-  const increment = useCallback(() => setCount((x) => x + 1), []);
-  return { count, increment };
+export default function useCounter(initValue = 0) {
+  const [count, setCount] = useState(initValue);
+  const increment = useCallback(() => setCount((x) => x + 1), [initValue]);
+  const reset = useCallback(() => setCount((x) => initValue), [initValue]);
+  return { count, increment, reset };
 }
-test("should use counter", () => {
-  const { result } = renderHook(() => useCounter());
-
-  expect(result.current.count).toBe(0);
-  expect(typeof result.current.increment).toBe("function");
+describe("should use counter", () => {
+  test("base", () => {
+    const { result } = renderHook(() => useCounter(100));
+    expect(result.current.count).toBe(100);
+    expect(typeof result.current.increment).toBe("function");
+    act(() => {
+      result.current.increment();
+    });
+    expect(result.current.count).not.toBe(100);
+    expect(result.current.count).toBe(101);
+  });
+  test("rerender", () => {
+    let initialValue = 0;
+    const { result, rerender } = renderHook(
+      ({ initialValue }) => useCounter(initialValue),
+      {
+        initialProps: { initialValue },
+      }
+    );
+    expect(result.current.count).toBe(0);
+    rerender({ initialValue: 100 });
+    act(() => {
+      result.current.reset();
+    });
+    expect(result.current.count).toBe(100);
+  });
 });
